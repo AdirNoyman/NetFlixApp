8910f9e87aae601393e113e36e367dc81226b0fb
Adir Noyman <ofahol2007@hotmail.com>
2024-09-02 22:42:18 +0300

    .

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -16,7 +16,7 @@ const AuthScreen = () => {
         />
         <Link
           to={'/login'}
-          className="bg-red-600 hover:bg-red-700 px-2 py-1 text-white rounded"
+          className="bg-red-600 hover:bg-red-700 px-3 py-2 text-white rounded"
         >
           Sign In
         </Link>
@@ -44,8 +44,31 @@ const AuthScreen = () => {
             Get Started
             <ChevronRight className="size-6 md:size-7" />
           </button>
-        </form>
+        </form>        
       </div>
+      {/* page body seperator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true">
+        {/* 1st section */}
+        <div className="py-10 bg-black text-white">
+          <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+            {/* left column */}
+            <div className="flex-1 text-center md:text-left">
+              <h2 className='text-4xl md:text-5xl font-extrabold mb-4'>
+                Enjoy Your TV
+              </h2>
+              <p className='text-lg md:text-xl'>Watch on Smart TVs, Playstation, Xbox, Chromcast,Apple TV, Blu-ray palyers and more.</p>
+            </div>
+            {/* right column */}
+            <div className="flex-1 relative">
+              <img src="/images/tv.png" alt="TV image" className='mt-4 z-20 relative'/>
+              <video className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1/2 z-10' playsInline autoPlay={true} muted loop>
+                <source src='/images/hero-vid.m4v' type='video/mp4' />
+              </video>
+            </div> 
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
